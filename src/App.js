cd0bca8cf1e0ce11da8ed48594a721acd4095a97
peck liu <xiaoyang20170204@gmail.com>
2017-12-01 23:59:18 +0800

    feature:create SearchBooks & ListBooks component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SearchBooks from './SearchBooks'
+import ListBooks from './ListBooks'
 import './App.css';
 
 class App extends Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+      {this.state.showSearchPage ? <SearchBooks/> : <ListBooks/>}
       </div>
-    );
+    )
   }
 }
 
