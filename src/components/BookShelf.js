e32badab23e834475a8a59461e7aee260bdf7bf5
peck liu <xiaoyang20170204@gmail.com>
2017-12-02 17:33:44 +0800

    refactor:replace hard code with remote server data for the currently reading shelf

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -2,39 +2,25 @@ import React, { Component } from 'react'
 import Book from './Book'
 
 class BookShelf extends Component {
-  state = {
-    name: this.props.name,
-    books: this.props.books
-  }
-
   render() {
     console.log(this.props.books);
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.state.name}</h2>
+        <h2 className="bookshelf-title">{this.props.name}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book/>
-            </li>
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">Ender's Game</div>
-                <div className="book-authors">Orson Scott Card</div>
-              </div>
-            </li>
+            {
+              this.props.books.map((book) => (
+                  <li key={ book.id }>
+                    <Book
+                      authors={ book.authors }
+                      title={ book.title }
+                      imageLinks={ book.imageLinks }
+                      shelf={ book.shelf }
+                    />
+                  </li>
+              ))
+            }
           </ol>
         </div>
       </div>
