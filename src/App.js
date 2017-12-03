b9d33e0151941373422ddbd3912180997ffafcee
peck liu <xiaoyang20170204@gmail.com>
2017-12-03 21:14:06 +0800

    docs:add comments for App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import './App.css'
 import * as BooksAPI from './utils/BooksAPI'
 import { Route } from 'react-router-dom';
 
+/**
+ *  @description
+ *  @method constructor constructor method
+ *  @method componentDidMount Call getAll() of BooksAPI to load initial books data form remote server
+ *  @method changeBookShelf Move books between different shelves & add searched books to shelves
+ */
 class App extends Component {
   constructor () {
     super();
@@ -15,21 +21,31 @@ class App extends Component {
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-      this.setState({ books: books })
+      this.setState({ books: books });
     })
   }
 
-  /* 改变书籍所属书架
-   * this.setState 更新本地状态
+  /**
+   *  @description
+   *  @param {Book} currentHandleBook This is a Book instance stand for the current book to be moved
+   *  @param {string} value The shelf books to be moved into
+   *  @param {array} books local Initial books data get form remote server
+   *  1. For local machine, get current book's index, get, change it in or add it into books array, then update local books array
+   *  2. For remote server, synchronize local updates to server by using use BooksAPI.update()
    */
   changeBookShelf = (currentHandleBook, value) => {
     const { books } = this.state;
 
+    /* get index of the book in local machine */
     let currentLocalBookIndex = books.findIndex((book) =>{
       return book.id === currentHandleBook.id
     })
 
-    /* 如果 currentLocalBookIndex < 0 说明本地没有这本书 */
+    /*
+     * if currentLocalBookIndex < 0 ,it's a searched book to be added in shelves that doesn't exsit in local machine
+     * just push it into local books array and update this books array
+     * otherwise, direct change its shelf and update the local books array
+     */
     if (currentLocalBookIndex < 0) {
       books.push(currentHandleBook);
       this.setState({ books: books });
@@ -39,6 +55,7 @@ class App extends Component {
       this.setState({ books: books });
     }
 
+    /* synchronize data to server */
     BooksAPI.update(books[currentLocalBookIndex], value)
   }
 
